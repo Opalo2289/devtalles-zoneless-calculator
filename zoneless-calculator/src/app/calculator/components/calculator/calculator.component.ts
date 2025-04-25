b4c2dbdef9be21849601d6a3ce077a694be49f2d
Opalo1189 <enriquemaestre11@gmail.com>
2025-04-25 06:44:17 -0500

    se divide en componentes

---
+++ zoneless-calculator/src/app/calculator/components/calculator/calculator.component.ts
@@ -0,0 +1,16 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { CalculatorButtonComponent } from '../calculator-button/calculator-button.component';
+
+@Component({
+  selector: 'calculator',
+  standalone: true,
+  imports: [
+    CalculatorButtonComponent
+  ],
+  templateUrl: './calculator.component.html',
+  styleUrl: './calculator.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export default class CalculatorComponent {
+
+}
